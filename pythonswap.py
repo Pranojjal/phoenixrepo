2086223aeffb7ecc5b90a1b033a2d8e424b61ba1
Pranojjal Roy <98149822+Pranojjal@users.noreply.github.com>
2023-10-28 11:25:14 +0530

    Create pythonswap.py

---
+++ pythonswap.py
@@ -0,0 +1,13 @@
+x = 5.4
+y = 10.3
+ 
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+ 
+
+x = x + y
+y = x - y 
+x = x - y 
+ 
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
